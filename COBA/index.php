722d75b0e4e8fd3ff4ae3ac1a920041f84f57905
anargya22 <136190231+anargya22@users.noreply.github.com>
2025-07-10 14:31:36 +0700

    Add files via upload

---
+++ COBA/index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <style>
+    table {
+      width: 60%;
+      border-collapse: collapse;
+      margin: 25px auto;
+      align: center;
+    }
+    thead, td {
+      border: 1px solid #ddd;
+      padding: 10px;
+      text-align: left;
+    }
+    thead {
+      background-color: #f5f5f5;
+    }
+    tr:nth-child(even) {
+      background-color: #f9f9f9;
+    }
+  </style>
+</head>
+<body>
+  <h1 style="text-align: center">Harga Buku</h1>
+  <table>
+    <thead style="text: bold">
+      <tr>
+        <td>No</td>
+        <td>Buku</td>
+        <td>Harga</td>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>1</td>
+        <td>No Game No Life</td>
+        <td>Rp 75.000,00</td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>Solo Leveling</td>
+        <td>Rp 100.000,00</td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>Financial Freedom</td>
+        <td>Rp 50.000,00</td>
+      </tr>
+      <tr>
+        <td>4</td>
+        <td>Perbankan</td>
+        <td>Rp 200.000,00</td>
+      </tr>
+    </tbody>
+  </table>
+</body>
+</html>
